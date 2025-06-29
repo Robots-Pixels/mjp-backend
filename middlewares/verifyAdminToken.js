75b878f0a835fe0087d2246a3060d01b50ceda80
Robots-Pixels <Robots-Pixels>
2025-06-29 07:51:13 +0100

    no Bearer

--- middlewares/verifyAdminToken.js
+++ middlewares/verifyAdminToken.js
@@ -1,21 +1,18 @@
+// middlewares/verifyAdminToken.js
 import jwt from 'jsonwebtoken';
 
 export const verifyAdminToken = (req, res, next) => {
-  const JWT_SECRET = process.env.JWT_SECRET;
-  const authHeader = req.headers.authorization;
+  const token = req.body.token || req.query.token;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({ status: false, message: "Accès non autorisé." });
+  if (!token) {
+    return res.status(403).json({ status: false, message: 'Token requis.' });
   }
-
-  const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.admin = decoded; 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded;
     next();
   } catch (err) {
-    return res.status(403).json({ status: false, message: "Token invalide ou expiré." });
+    return res.status(401).json({ status: false, message: 'Token invalide.' });
   }
 };
-

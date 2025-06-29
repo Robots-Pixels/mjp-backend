b0f5e56ea2d00eef17024cc2cb6d47727bab9704
Robots-Pixels <Robots-Pixels>
2025-06-29 04:53:26 +0100

    Addind OPTIONS for preflight

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors({
   origin: ['http://127.0.0.1:5500', 'https://mjp-admin.vercel.app', 'https://mjp-signup.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 

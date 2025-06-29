eba4a881c005d81a85ed312686e4c4ecc010f78c
Robots-Pixels <Robots-Pixels>
2025-06-29 07:29:07 +0100

    cors 5

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: ["*"]
-}));
+const corsOptions = {
+    origin: ['*'],
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  };
   
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 const PORT = process.env.PORT

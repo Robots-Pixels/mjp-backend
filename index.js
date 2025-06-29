75ed4ce4523b9583cabf437388a2232ccb4a0801
Robots-Pixels <Robots-Pixels>
2025-06-29 04:59:25 +0100

    Cors 2

--- index.js
+++ index.js
@@ -9,12 +9,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'https://mjp-admin.vercel.app', 'https://mjp-signup.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(cors());
+  
 app.use(express.json());
 
 const PORT = process.env.PORT
